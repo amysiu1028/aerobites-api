4e5e3474282d3f05d2d770f9759a263eb45d148a
Emalee Poellot <emalee.poellot@gmail.com>
2024-01-02 15:49:20 -0700

    feat/Add airport data and begin adding data for terminals

--- data/airport.js
+++ data/airport.js
@@ -1,2 +1,76 @@
 //how to organize data with seeds!
 
+const airportData = [
+    {
+    "id": 1,
+    "name": "Hartsfield-Jackson Atlanta International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 2,
+    "name": "Dallas-Fort Worth International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 3,
+    "name": "Denver International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 4,
+    "name": "Chicago’s O’Hare International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 5,
+    "name": "Harry Reid International Airport in Las Vegas",
+    "isFavorite": false
+    },
+    {
+    "id": 6,
+    "name": "Orlando International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 7,
+    "name": "Miami International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 8,
+    "name": "San Francisco International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 9,
+    "name": "Houston’s George Bush Intercontinental Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 10,
+    "name": "Boston’s General Edward Lawrence Logan International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 11,
+    "name": "Fort Lauderdale/Hollywood International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 12,
+    "name": "Minneapolis–St. Paul International Airport",
+    "isFavorite": false
+    },
+    {
+    "id": 13,
+    "name": "LaGuardia Airport in New York",
+    "isFavorite": false
+    },
+    {
+    "id": 14,
+    "name": "Detroit Metro Wayne County Airport",
+    "isFavorite": false
+    }
+    ];
+
+    export { airportData }
---
+++ data/businesses.js
@@ -0,0 +1,652 @@
+[
+    {
+    "id": 1,
+    "terminal_id": 1,
+    "businessName": "Atlanta Chophouse"
+    },
+    {
+    "id": 2,
+    "terminal_id": 1,
+    "businessName": "Auntie Anne's"
+    },
+    {
+    "id": 3,
+    "terminal_id": 1,
+    "businessName": "Burger King"
+    },
+    {
+    "id": 4,
+    "terminal_id": 1,
+    "businessName": "IHOP express"
+    },
+    {
+    "id": 5,
+    "terminal_id": 1,
+    "businessName": "Starbucks"
+    },
+    {
+    "id": 6,
+    "terminal_id": 1,
+    "businessName": "TGI Fridays'"
+    },
+    {
+    "id": 7,
+    "terminal_id": 1,
+    "businessName": "We Juice It"
+    },
+    {
+    "id": 8,
+    "terminal_id": 1,
+    "businessName": "Z Market"
+    },
+    {
+    "id": 9,
+    "terminal_id": 2,
+    "businessName": ""
+    },
+    {
+    "id": 10,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 11,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 12,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 13,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 14,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 15,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 16,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 17,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 18,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 19,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 20,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 21,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 22,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 23,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 24,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 25,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 26,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 27,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 28,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 29,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 30,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 31,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 32,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 33,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 34,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 35,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 36,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 37,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 38,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 39,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 40,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 41,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 42,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 43,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 44,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 45,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 46,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 47,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 48,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 49,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 50,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 51,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 52,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 53,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 54,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 55,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 56,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 57,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 58,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 59,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 60,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 61,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 62,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 63,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 64,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 65,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 66,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 67,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 68,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 69,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 70,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 71,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": 72,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    {
+    "id": ,
+    "terminal_id": ,
+    "businessName": ""
+    },
+    ]
